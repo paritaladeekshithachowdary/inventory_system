cce8af85dc1eadb3dc08b4e521a8ad6aaa317e53
paritaladeekshithachowdary <paritaladeekshithachowdary@gmail.com>
2026-04-16 02:20:46 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-$host = "containers-us-west-xxx.railway.app"; // your PUBLIC host
-$username = "root";                            // your username
-$password = "your_password";                   // your password
-$database = "railway";                         // your database name
-$port = 6543;                                  // your port
+$host = "turntable.proxy.rlwy.net";
+$username = "root";
+$password = "bxcyizHeRtKMmIiCqoEFCkPensTLLMag";
+$database = "railway";
+$port = 47115;
 
 $conn = new mysqli($host, $username, $password, $database, $port);
 
