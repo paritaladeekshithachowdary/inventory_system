4f9c2a87cf257fcad2757746e65b633fbef955fe
paritaladeekshithachowdary <paritaladeekshithachowdary@gmail.com>
2026-04-16 02:48:21 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,24 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $host = "turntable.proxy.rlwy.net";
+$dbname = "railway";
 $username = "root";
 $password = "bxcyizHeRtKMmIiCqoEFCkPensTLLMag";
-$database = "railway";
 $port = 47115;
 
-$conn = new mysqli($host, $username, $password, $database, $port);
+try {
+    $conn = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname",
+        $username,
+        $password
+    );
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    // Enable error mode
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
