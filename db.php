325ee70dce74b061ada55a71c170805650c39696
paritaladeekshithachowdary <paritaladeekshithachowdary@gmail.com>
2026-04-16 01:55:48 +0530

    Update database connection details in db.php

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
-$conn = new mysqli(
-    "mysql.railway.internal",
-    "root",
-    "bxcyizHeRtKMmIiCqoEFCkPensTLLMag",
-    "railway",
-    3306
-);
+$host = "containers-us-west-xxx.railway.app"; // your PUBLIC host
+$username = "root";                            // your username
+$password = "your_password";                   // your password
+$database = "railway";                         // your database name
+$port = 6543;                                  // your port
 
+$conn = new mysqli($host, $username, $password, $database, $port);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
